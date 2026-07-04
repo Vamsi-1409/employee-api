aa0e8ec5f4ebe7a8c40813e202694f0019ad52f9
Vamsi-1409 <vamsigudla2002@gmail.com>
2026-07-04 20:33:20 +0530

    fix format error

--- database/postgres.go
+++ database/postgres.go
@@ -18,7 +18,7 @@ func Connect(host, port, user, password, dbname string, sslmode string) error {
 		user,
 		password,
 		dbname,
-                dbsslmode,
+		dbsslmode,
 	)
 
 	db, err := sql.Open("postgres", connStr)
